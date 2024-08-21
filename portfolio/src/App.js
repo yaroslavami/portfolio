88c5613f2266c8766d01ad0cef13716b73c7fccb
Yaroslava <yaroslavamishsce@gmail.com>
2024-08-20 23:43:17 -0400

    added router dom

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+// import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      {/* <Router>
+        <Routes>
+          <Route path="/" element={} />
+        </Routes>
+      </Router> */}
+    </div>
   );
 }
 
